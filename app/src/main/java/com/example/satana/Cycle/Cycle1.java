cfc54c63a26fe18293825e529e41f654f6f595f0
Khorn <decardcain21@gmail.com>
2019-05-11 22:36:21 +0300

    commit about refactor

--- app/src/main/java/com/example/satana/Cycle/Cycle1.java
+++ app/src/main/java/com/example/satana/Cycle/Cycle1.java
@@ -3,7 +3,6 @@ package com.example.satana.Cycle;
 public class Cycle1 {
     public static void main(String[] args) {
 
-        //todo refactor
         for (int i = 0; i < 257; i++ ) {      //Выведит 4 иттерации т.к с 0?В С по-другому вроде
             System.out.println(i);
         }
--- app/src/main/java/com/example/satana/Cycle/Cycle2.java
+++ app/src/main/java/com/example/satana/Cycle/Cycle2.java
@@ -4,8 +4,8 @@ public class Cycle2 {
     public static void main(String[] args) {
         int i = 0;
         while (i <= 257) {
-            i++;
-            if(i%2 != 0) //==0 чётные
+            i++; //
+            if(i%2 != 0) //Q+alt+K
             System.out.println(i);
         }
     }
