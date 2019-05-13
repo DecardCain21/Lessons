077dd7aad0f5c9665285787591f8a1a4297a4805
Khorn <decardcain21@gmail.com>
2019-05-13 22:04:57 +0300

    Они почему-то красные...я забыл почему,кажется ты говорил,иии это задания вплодь до 5,первые 3 нужно было сделать проще(я не понял что нужно в 4?Write a Java program to print the result of the following operations.Просто вывести результат?)

---
+++ app/src/main/java/com/example/satana/Exercise/Ex3.java
@@ -0,0 +1,22 @@
+package com.example.satana.Exercise;
+
+import java.util.Scanner;
+
+public class Ex3 {
+    public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int numb1 = 0;
+            System.out.print("Press number 2 : ");
+            numb1 = scan.nextInt();
+            int numb2;
+            System.out.print("Press number 2 : ");
+            numb2 = scan.nextInt();
+            {
+                System.out.println(numb1 / numb2);
+//                Походу это задания вплодь до 5...первые три
+//                нужно было делать проще чем подумал я
+            }
+        }
+    }
+
+
