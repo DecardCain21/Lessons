2a11190f529e17b1a213864acd2fb2cd35bc3b6f
Khorn <decardcain21@gmail.com>
2019-05-23 12:58:42 +0300

    Заливаю

---
+++ app/src/main/java/com/example/satana/Exercise/Ex7.java
@@ -0,0 +1,36 @@
+package com.example.satana.Exercise;
+
+import java.util.Scanner;
+
+public class Ex7 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double numb1;
+
+        System.out.print("Press number 1 : ");
+        numb1 = scan.nextDouble();
+        double numb2;
+
+        System.out.print("Press number 1 : ");
+        numb2 = scan.nextDouble();
+        double numb3;
+
+        System.out.print("Press number 1 : ");
+        numb3 = scan.nextDouble();
+        double numb4;
+
+        System.out.print("Press number 1 : ");
+        numb4 = scan.nextDouble();
+        double numb5;
+
+        System.out.print("Press number 1 : ");
+        numb5 = scan.nextDouble();
+
+        double numb6;
+        System.out.print("Press number 1 : ");
+        numb6 = scan.nextDouble();
+        {
+            System.out.println((numb1 * numb2 - numb3 * numb4) / (numb5 - numb6));
+        }
+    }
+}
