b8b2ca2160164607dfe1cd96a3ced0a36ba902bd
Khorn <decardcain21@gmail.com>
2019-05-23 14:23:55 +0300

    Merge branch 'master' of D:\Satana with conflicts.

--- app/src/main/java/com/example/satana/Exercise/Ex2.java
+++ app/src/main/java/com/example/satana/Exercise/Ex2.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class Ex2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int numb1;//не обязательно обнулять примитив, компилятор по дефолту и так даст ему нуль
+        int numb1 = 0;
         System.out.print("Press number 1 : ");
         numb1 = scan.nextInt();
         int numb2;
         System.out.print("Press number 2 : ");
         numb2 = scan.nextInt();
-        System.out.println(numb1 + numb2); //зачем ты написал эту строчку в блоке ? тут нет смысла
+        {
+            System.out.println(numb1 + numb2);
+        }
     }
 }
