1f9da54094b67a71dd1c7ffab48476365c8814b8
andreyLebedev <andrey.l@zemingo.com>
2019-06-06 11:33:39 +0300

    andrey: Comments for ex 9

--- app/src/main/java/com/example/satana/Exercise/Ex9.java
+++ app/src/main/java/com/example/satana/Exercise/Ex9.java
@@ -4,7 +4,12 @@ import java.util.Scanner;
 
 public class Ex9 {
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
+
+        //todo Марат добавочное задание к этому
+       /* сделай так чтоб можно было вводить бесконечное колличевство цифр,
+                но когда ты вводишь букву(что то что не цифра)
+        он считает среднее всех введеных чисел*/
+        Scanner scan = new Scanner(System.in);
         double numb1;
         System.out.print("Press number 1 : ");
         numb1 = scan.nextDouble();
@@ -15,7 +20,7 @@ public class Ex9 {
         System.out.print("Press number 3 : ");
         numb3 = scan.nextDouble();
 
-        double summ=(numb1+numb2+numb3)/3;
+        double summ = (numb1 + numb2 + numb3) / 3;
         System.out.println(summ);
     }
 }
