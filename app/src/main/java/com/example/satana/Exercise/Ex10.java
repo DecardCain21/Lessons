94022c1d666ad4cd1bb34edd7e5e2f11b02a7891
Khorn <decardcain21@gmail.com>
2019-06-29 10:45:50 +0300

    Просто реверс,делаю бесконечный

---
+++ app/src/main/java/com/example/satana/Exercise/Ex10.java
@@ -0,0 +1,23 @@
+package com.example.satana.Exercise;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        int x = 42357;
+        int reverse;
+        int z;
+        do {
+            z = +10;
+            reverse = x % z;
+            System.out.println(reverse);
+            x = x / 10;
+        }
+            while (x > 0);
+        }
+
+    }
+
+
+
+
+
+
