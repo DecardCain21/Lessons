d9b64aa1aba55be3bbd3c88889fc7f29cf59761e
Khorn <decardcain21@gmail.com>
2019-07-08 23:36:17 +0300

    Полиндром хотов

---
+++ app/src/main/java/com/example/satana/Exercise/Sotki.java
@@ -0,0 +1,22 @@
+package com.example.satana.Exercise;
+
+import java.util.Scanner;
+
+public class Sotki {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int Chislo;
+        int Ostatok = 0;
+        int i = 0;
+        System.out.println("Press ur number");
+        Chislo = scan.nextInt();
+        while (i < 3) {
+            Ostatok = Chislo % 10;
+            Chislo = Chislo / 10;
+            i++;
+            }
+            System.out.println("This is hundreds: " + Ostatok );
+            scan.close();
+        }
+    }
+
