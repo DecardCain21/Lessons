53712447a3d110ec4d0196031efc5dc8d1ee8626
Khorn <decardcain21@gmail.com>
2019-07-20 19:15:03 +0300

    Между двумя введёными числами,не опт

---
+++ app/src/main/java/com/example/satana/Exercise/LineOfNumbs.java
@@ -0,0 +1,28 @@
+package com.example.satana.Exercise;
+
+import java.util.Scanner;
+
+public class LineOfNumbs {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int numb1;
+        System.out.println("Введите первое число");
+        numb1=scan.nextInt();
+        int numb2;
+        System.out.println("Введите второе число");
+        numb2=scan.nextInt();
+        if(numb1<numb2){
+            System.out.println("Между введёными числами:");
+            for(;numb1<=numb2;numb1++){
+                System.out.print(numb1+" ");
+            }
+        }
+        if (numb1 > numb2) {
+            System.out.print("Между введёными числами:");
+            for(;numb1>=numb2;numb1--){
+                    System.out.print(numb1+" ");
+            }
+        }
+
+    }
+}
