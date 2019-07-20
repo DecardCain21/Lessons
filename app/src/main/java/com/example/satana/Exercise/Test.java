d540ec47ea19f7075d834806cf61934a2a7ac3ae
Khorn <decardcain21@gmail.com>
2019-07-20 17:57:35 +0300

    Calculate?

--- app/src/main/java/com/example/satana/Exercise/Test.java
+++ app/src/main/java/com/example/satana/Exercise/Test.java
@@ -8,47 +8,33 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        List<Integer> masseleven = new ArrayList<Integer>();
-        List<Integer> massseven = new ArrayList<Integer>();
-        List<Integer> massfive = new ArrayList<Integer>();
-        List<Integer> massthree = new ArrayList<Integer>();
-        List<Integer> masstwo = new ArrayList<Integer>();
-        int i = 0;
-        int z;
-        do {
-            System.out.println("Press ur number");
-            int chislo = scan.nextInt();
-            i++;
-            switch (chislo){
-                case 2:
-                    i++;
-                int two = chislo;
-                masstwo.add(chislo);
-                case 3:
-                    i++;
-                int three = chislo;
-                massthree.add(three);
-                case 5:
-                    i++;
-                int five = chislo;
-                massfive.add(five);
-                case 7:
-                    i++;
-                int seven = chislo;
-                massseven.add(seven);
-                case 11:
-                    i++;
-                int eleven = chislo;
-                masseleven.add(eleven);
-                System.out.println(masseleven);
-            }
-        }
-        while (i < 10);
-        System.out.println("Числа делённые на 2:"+masstwo+"\nЧисла делённые на 3:"+massthree+"\nЧисла делённые на 5:"+massfive+"\nЧисла делённые на 7:"+massseven+"\nЧисла делённые на 11:"+masseleven);
+        char[] array = new char[5];
+        array[0]='%';
+        array[1]='/';
+        array[2]='*';
+        array[3]='+';
+        array[4]='-';
+        System.out.println("Введите 1-ое число:");
+        int numb1 = scan.nextInt();
+        System.out.println("Введите оператора:");
+        char operator = scan.next().charAt(0);
+        System.out.println("Введите 2-ое число:");
+        int numb2 = scan.nextInt();
+        if (array[0]== operator)
+            System.out.println("Итого:"+(numb1%numb2));
+
+        if(array[1]==operator)
+            System.out.println("Итого:"+(numb1/numb2));
+
+        if(array[2]==operator)
+            System.out.println("Итого:"+(numb1*numb2));
+
+        if(array[3]==operator)
+            System.out.println("Итого:"+(numb1+numb2));
+        if(array[4]==operator)
+            System.out.println("Итого:"+(numb1-numb2));
     }
 }
-
-
 
 //        else if (gamenumber < numb - 7) {
 //        System.out.println("Холодно1");
@@ -60,3 +46,4 @@ public class Test {
 //            if (chislo < 10) {
 //                System.out.println("Введите число chislo>=10");
 //            }
+//ОР System.out.println("Итого:"+numb1+numb2);
