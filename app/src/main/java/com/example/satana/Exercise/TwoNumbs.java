0a22fbf2828e1de1f79f9d78ca22c9ca7659aea7
Khorn <decardcain21@gmail.com>
2019-07-30 15:49:48 +0300

    Заморочился :D

--- app/src/main/java/com/example/satana/Exercise/TwoNumbs.java
+++ app/src/main/java/com/example/satana/Exercise/TwoNumbs.java
@@ -14,33 +14,29 @@ public class TwoNumbs {
         int numb1 = scan.nextInt();
         System.out.println("Введите 2-ое число:");
         int numb2 = scan.nextInt();
-        int Ostatok;
+        int Ostatok ;
         int Ostatok2;
         List<Integer> numbList1 = new ArrayList<Integer>();
         List<Integer> numbList2 = new ArrayList<Integer>();
-        int i=0;
-        while (i<4) {
-            i++;
+        while (numb1 >= 1) {
             Ostatok = numb1 % 10;
             numb1 = numb1 / 10;
             numbList1.add(Ostatok);
+        }
+        while (numb2 >= 1) {
             Ostatok2 = numb2 % 10;
             numb2 = numb2 / 10;
             numbList2.add(Ostatok2);
-            numbList1.toArray();
-            numbList2.toArray();
-            System.out.println(numbList1);
-            System.out.println(numbList2);
-            Collections.sort(numbList1);
-            Collections.sort(numbList2);
-            if (Ostatok==Ostatok2) {
-                System.out.println("Есть одинаковые цифры"+numbList1.equals(numbList2));
-                break;
-            }
-            else{
-                System.out.println("Нет одинаковых цифр");
+        }
+        Collections.sort(numbList1);
+        Collections.sort(numbList2);
+        for (int i = 0; i < numbList1.size(); i++) {
+            for (int j = 0; j < numbList1.size(); j++) {
+                if (numbList1.get(i).equals(numbList2.get(j)) == true) {
+                    System.out.println(numbList1.get(i) == (numbList2.get(j)) == true);
+                    break;
+                }
             }
         }
     }
 }
-
