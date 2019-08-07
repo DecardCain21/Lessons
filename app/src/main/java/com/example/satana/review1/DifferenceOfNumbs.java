b67d792330c641acc9d98427de865c370adbed1c
Khorn <decardcain21@gmail.com>
2019-08-07 14:16:55 +0300

    Переделанные методы

--- app/src/main/java/com/example/satana/review1/DifferenceOfNumbs.java
+++ app/src/main/java/com/example/satana/review1/DifferenceOfNumbs.java
@@ -1,6 +1,7 @@
 package com.example.satana.review1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
@@ -8,31 +9,15 @@ import static java.lang.System.in;
 
 public class DifferenceOfNumbs {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(in);
-        List<Integer> numbList1 = new ArrayList<Integer>();
-        List<Integer> numbList = numbList1;
-        int chislo = enterNum(scan);
-        int result = summ(scan, chislo);
+        int[] massiv ={1,23,3,45,5};
+        int result = summ(massiv);
         System.out.println(result);
     }
 
-    private static int enterNum(Scanner scan) {
-        System.out.println("Введите число:");
-        return scan.nextInt();
-    }
-
-    private static int summ(Scanner scan, int numb) {
-        List<Integer> numbList1 = new ArrayList<Integer>();
-        int result=0;
-        int i=0;
-        numbList1.add(numb);
-        while (i<4) {
-            i++;
-            numb = scan.nextInt();
-            numbList1.add(numb);
-            result = Collections.max(numbList1) - Collections.min(numbList1);
-        }
-        Collections.sort(numbList1);
+    private static int summ(int []massiv) {
+        int result;
+        Arrays.sort(massiv);
+        result = massiv[4] - massiv[0];
         return result;
     }
 }
--- app/src/main/java/com/example/satana/review1/exSummOfTwoNumbs.java
+++ app/src/main/java/com/example/satana/review1/exSummOfTwoNumbs.java
@@ -7,28 +7,21 @@ import static java.lang.System.in;
 
 public class exSummOfTwoNumbs {
         public static void main(String[] args) {
-            Scanner scan = new Scanner(in);
-            int chislo=enterNum(scan);
-            int result=summ(scan,chislo);
+            int[] massiv={4,6,1,3,4};
+            int result=summ(massiv);
             System.out.println(result);
-            scan.close();
-        }
-        private static int enterNum(Scanner scan) {
-            System.out.println("Введите число:");
-            return scan.nextInt();
         }
 
-        private static int summ(Scanner scan,int numb1) {
-            List<Integer> massiv = new ArrayList<Integer>();
-            int i = 0;
-            int result;
-            massiv.add(numb1);
-            while (i < 2) {
-                i++;;
-                numb1=scan.nextInt();
-                massiv.add(numb1);
+        private static int summ(int[] massiv) {
+            int result=10;
+            if(massiv.length<2){
+                result=massiv[0];
+                return result;
             }
-            result=massiv.get(0)+massiv.get(1);
+            if(massiv.length>2){
+                result=massiv[0]+massiv[1];
+                return result;
+            }
             return result;
         }
     }
