bc923400194c3df163f8752dbd126d0650b8a2e3
andreyLebedev <andrey.l@zemingo.com>
2019-08-09 22:04:24 +0300

    exr1 refactor

--- app/src/main/java/com/example/satana/review1/exSummOfTwoNumbs.java
+++ app/src/main/java/com/example/satana/review1/exSummOfTwoNumbs.java
@@ -1,28 +1,18 @@
 package com.example.satana.review1;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import static java.lang.System.in;
-
 public class exSummOfTwoNumbs {
-        public static void main(String[] args) {
-            int[] massiv={4,6,1,3,4};
-            int result=summ(massiv);
-            System.out.println(result);
-        }
+    public static void main(String[] args) {
+        int[] massiv = {4, 6, 1, 3, 4};
+        int result = summ(massiv);
+        System.out.println(result);
+    }
 
-        private static int summ(int[] massiv) {
-            int result=10;
-            if(massiv.length<2){
-                result=massiv[0];
-                return result;
-            }
-            if(massiv.length>2){
-                result=massiv[0]+massiv[1];
-                return result;
-            }
-            return result;
+    private static int summ(int[] massiv) {
+        if (massiv.length < 2) {
+            return massiv[0];
+        } else {
+            return massiv[0] + massiv[1];
         }
     }
+}
 
