38768090802fd8e1cb57402a84f82da7cbecf18c
Khorn <decardcain21@gmail.com>
2019-08-09 22:29:32 +0300

    Ex5,метод

--- app/src/main/java/com/example/satana/review1/ValueOfStringEx5.java
+++ app/src/main/java/com/example/satana/review1/ValueOfStringEx5.java
@@ -5,24 +5,28 @@ import java.util.Scanner;
 
 public class ValueOfStringEx5 {
     public static void main(String[] args) {
-        HashMap<String, Integer> store = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
         int z = 0;
-        while (z < 10) {
-            z++;
-            System.out.println("Введите целое чилсло");
-            String key = scanner.nextLine();
-            Integer counter = store.get(key);
-            if (counter == null) {
-                counter = 0;
-            }
-            counter++;
-            store.put(key, counter);
-        }
-        scanner.close();
+        HashMap<String, Integer> store = store2(scanner, z);
         for (String i : store.keySet()) {
             System.out.println("число: " + i + " введено колл во раз: " + store.get(i));
         }
         System.out.println("Bye bye");
+    }
+
+    private static HashMap<String,Integer> store2(Scanner scanner,int z) {
+        HashMap<String, Integer> store2 = new HashMap<>();
+        while (z < 10) {
+            z++;
+            System.out.println("Введите целое чилсло");
+            String key = scanner.nextLine();
+            Integer counter = store2.get(key);
+            if (counter == null) {
+                counter = 0;
+            }
+            counter++;
+            store2.put(key, counter);
+        }
+        return store2;
     }
 }
