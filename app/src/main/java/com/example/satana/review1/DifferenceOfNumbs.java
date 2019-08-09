92e2e198225f8c661dbab29d07ed234f8f2bd8c5
andreyLebedev <andrey.l@zemingo.com>
2019-08-09 22:02:54 +0300

    additional exercise for assignment 2

--- app/src/main/java/com/example/satana/review1/DifferenceOfNumbs.java
+++ app/src/main/java/com/example/satana/review1/DifferenceOfNumbs.java
@@ -1,25 +1,23 @@
 package com.example.satana.review1;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
-import static java.lang.System.in;
 
 public class DifferenceOfNumbs {
     public static void main(String[] args) {
-        int[] massiv ={1,23,3,45,5};
-        int result = summ(massiv);
+        int[] massiv = {1,23,3,2};
+        int result = different(massiv);
         System.out.println(result);
     }
 
-    private static int summ(int []massiv) {
+    private static int different(int[] massiv) {
         int result;
         Arrays.sort(massiv);
-        result = massiv[4] - massiv[0];
+        result = massiv[massiv.length-1] - massiv[0];
         return result;
     }
+
+    //сдеалать без сортировки
+
 }
 
 
