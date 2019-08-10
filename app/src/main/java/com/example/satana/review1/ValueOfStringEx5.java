e9ae5f7eec17bc3fea2735f91809b901d69071e4
Khorn <decardcain21@gmail.com>
2019-08-10 14:20:29 +0300

    Ex5,метод передел

--- app/src/main/java/com/example/satana/review1/ValueOfStringEx5.java
+++ app/src/main/java/com/example/satana/review1/ValueOfStringEx5.java
@@ -1,31 +1,34 @@
 package com.example.satana.review1;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class ValueOfStringEx5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int z = 0;
-        HashMap<String, Integer> store = store2(scanner, z);
+        ArrayList<String> key=new ArrayList<>();
+        ArrayList pay=key;
+        HashMap<String, Integer> store = store2(pay,scanner);
         for (String i : store.keySet()) {
-            System.out.println("число: " + i + " введено колл во раз: " + store.get(i));
+            System.out.println("Символ: " + i + " введено колл во раз: " + store.get(i));
         }
         System.out.println("Bye bye");
     }
 
-    private static HashMap<String,Integer> store2(Scanner scanner,int z) {
+    private static HashMap<String,Integer> store2(ArrayList<String>key,Scanner scanner) {
         HashMap<String, Integer> store2 = new HashMap<>();
+        int z = 0;
         while (z < 10) {
             z++;
-            System.out.println("Введите целое чилсло");
-            String key = scanner.nextLine();
-            Integer counter = store2.get(key);
+            System.out.println("Введите символ:");
+            String simvol=scanner.nextLine();
+            Integer counter = store2.get(simvol);
             if (counter == null) {
                 counter = 0;
             }
             counter++;
-            store2.put(key, counter);
+            store2.put(simvol, counter);
         }
         return store2;
     }
