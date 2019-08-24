4cf691543b2068c3f3101fad742a2486c5b95c3a
Khorn <decardcain21@gmail.com>
2019-08-24 23:45:09 +0300

    ex7

--- app/src/main/java/com/example/satana/ArraysPack/RasminkaEx7.java
+++ app/src/main/java/com/example/satana/ArraysPack/RasminkaEx7.java
@@ -6,7 +6,6 @@ public class RasminkaEx7 {
     public static void main(String[] args) {
         int[] massiv = new int[10];
         for (int i = 1; i < massiv.length; i++) {
-            massiv[i]=i;
             massiv[i]=i*i;
             System.out.println(Arrays.toString(massiv));
         }
