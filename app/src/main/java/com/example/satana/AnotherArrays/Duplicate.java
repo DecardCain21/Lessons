0c4b1e617f125e8c6cdbb624ad46fe5e5340dae2
Khorn <decardcain21@gmail.com>
2019-09-11 22:04:35 +0300

    Дубликаты

--- app/src/main/java/com/example/satana/AnotherArrays/Duplicate.java
+++ app/src/main/java/com/example/satana/AnotherArrays/Duplicate.java
@@ -1,16 +1,30 @@
 package com.example.satana.AnotherArrays;
 
+import android.renderscript.Sampler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Duplicate {
     public static void main(String[] args) {
-        int duplicate=0;
-        int[] array = {4,10,10,4,9,2,5,7,5};
-        for(int i=0;i<array.length;i++){
-            for(int z=0;z<i;z++){
-                if(array[i]==array[z]){
-                    duplicate++;
-                }
+        Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+        Integer key;
+        Integer counter;
+        int[] array = {4, 10, 10, 4, 9, 2, 5, 7, 5};
+        for (int i1 : array) {
+            key = i1;
+            counter = hashMap.get(key);
+            if(counter==null){
+                counter=0;
             }
+            counter++;
+            hashMap.put(key, counter);
         }
-        System.out.println("Number of duplicate:"+duplicate);
+        for (Integer i : hashMap.keySet()) {
+            System.out.println("Ключ|Число:" + i + "  Кол-во дубликатов: " + hashMap.get(i));
+        }
+
     }
 }
+//сделать одним for
