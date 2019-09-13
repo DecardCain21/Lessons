34e80528e91b646e5a2dd8127ccf7148be65589a
Khorn <decardcain21@gmail.com>
2019-09-13 19:54:43 +0300

    Что-то такое7

--- app/src/main/java/com/example/satana/AnotherArrays/SummOfElements.java
+++ app/src/main/java/com/example/satana/AnotherArrays/SummOfElements.java
@@ -1,17 +1,38 @@
 package com.example.satana.AnotherArrays;
 
+import java.util.ArrayList;
+
 public class SummOfElements {
     public static void main(String[] args) {
-        int summ=Integer.MIN_VALUE;
-        int[]array={1,2,9,4,20,1,7,8,2,10};
+        ArrayList<Integer> list =new ArrayList<>();
+        int summ=0;
+        int[]array={1,2,3,4,5,1,7,8,2,6,3,8,10,0};
         for(int i=0;i<array.length;i++){
-            if(i>=array.length-2){
-                System.out.println("Максимальная сумма из трёх последовательных чисел::"+summ);
+            if(i>=array.length-1){
                 break;
             }
-            if(array[i]+array[i+1]+array[i+2]>summ){
-                summ=array[i]+array[i+1]+array[i+2];
+            if(array[i]==array[i+1]-1||array[i]==array[i-1]+1){
+                list.add(array[i]);
+                System.out.println("Последовательное число:"+array[i]);
             }
         }
+        int max=Integer.MIN_VALUE;
+        for(Integer z:list){
+                if(list.size(z)>max){
+                    max=list.size();
+                }
+        }
+        System.out.println("Максимальная последовательность"+max);
     }
 }
+//три последовательных элемнта в смысле 1,2,3...9,10,11...сделать без сортировки
+// int summ=Integer.MIN_VALUE;
+//        int[]array={1,2,9,4,20,1,7,8,2,10};
+//        for(int i=0;i<array.length-2;i++){
+//            int i1 = array[i] + array[i + 1] + array[i + 2];
+//            if(i1 >summ){
+//                summ= i1;
+//            }
+//        }
+//    }
+//}
